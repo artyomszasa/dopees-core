e8df630afeb1820582926ecacc45db987112cf06
Artyom Antyipin <artyom@skape.io>
2018-12-20 11:33:52 +0100

    add message event subscription.

--- lib/core.d.ts
+++ lib/core.d.ts
@@ -4,3 +4,7 @@ export interface LogMessageData {
 }
 export declare function pushMsg(msg: any, severity: 'log' | 'warn' | 'error'): void;
 export declare function pushErr(msg: any): void;
+export declare function offMessage(callback: (message: LogMessageData) => void): void;
+export declare function onMessage(callback: (message: LogMessageData) => void): {
+    remove: () => void;
+};
--- src/core.ts
+++ src/core.ts
@@ -17,12 +17,30 @@ export function pushMsg(msg: any, severity: 'log'|'warn'|'error') {
 
 export function pushErr(msg: any) { pushMsg(msg, 'error'); }
 
-window.addEventListener(logEvent, e => {
-  const evt = <CustomEvent<LogMessageData>>e;
-  if (window.console) {
-      const data = evt.detail;
-      const severity = data.severity;
-      const log = window.console[severity] || window.console.log || (() => {});
-      log.call(window.console, data.message);
+const callbacks: Map<Function, Function> = new Map();
+
+export function offMessage(callback: (message: LogMessageData) => void) {
+  const cb = callbacks.get(callback);
+  if (cb) {
+    callbacks.delete(callback);
+    window.removeEventListener(logEvent, <any>cb, false);
   }
-}, false);
+}
+
+export function onMessage(callback: (message: LogMessageData) => void) {
+  const cb = (e: CustomEvent<LogMessageData>) => callback(e.detail);
+  callbacks.set(callback, cb);
+  window.addEventListener(logEvent, <any>cb, false);
+  return {
+    remove: () => offMessage(callback)
+  };
+}
+
+
+onMessage(data => {
+  if (window.console) {
+    const severity = data.severity;
+    const log = window.console[severity] || window.console.log || (() => {});
+    log.call(window.console, data.message);
+  }
+})
